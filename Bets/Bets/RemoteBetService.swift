ee076ca55e09490c71d9b4343657be4592cc3575
Aitor Lucas <aitor.eler.lucas@gmail.com>
2025-07-10 20:19:35 -0300

    refactor: auto lint

--- Bets/Bets/RemoteBetService.swift
+++ Bets/Bets/RemoteBetService.swift
@@ -2,11 +2,11 @@ import BetsCore
 
 actor RemoteBetService: BetService {
     private static let delay: UInt64 = 1_000_000_000
-    
+
     public static let instance = RemoteBetService()
-    
+
     private init() { }
-    
+
     private var bets: [Bet] = [
         Bet(name: "Winning team", sellIn: 8, quality: 15),
         Bet(name: "Total score", sellIn: 5, quality: 26),
@@ -23,14 +23,14 @@ actor RemoteBetService: BetService {
         Bet(name: "Hat trick", sellIn: 9, quality: 32),
         Bet(name: "Number of sets won", sellIn: 8, quality: 17),
         Bet(name: "Number of aces", sellIn: 6, quality: 46),
-        Bet(name: "Set score", sellIn: 9, quality: 12),
+        Bet(name: "Set score", sellIn: 9, quality: 12)
     ]
-    
+
     func loadBets() async throws -> [Bet] {
         try await Task.sleep(nanoseconds: RemoteBetService.delay)
         return bets
     }
-    
+
     func saveBets(_ bets: [Bet]) async throws {
         try await Task.sleep(nanoseconds: RemoteBetService.delay)
         self.bets = bets
--- Bets/Bets/ViewController.swift
+++ Bets/Bets/ViewController.swift
@@ -2,23 +2,23 @@ import UIKit
 import BetsCore
 
 class ViewController: UIViewController, UICollectionViewDataSource {
-    
+
     private var list: UICollectionView!
     private var activity: UIActivityIndicatorView!
-    
+
     private let repository = BetRepository(service: RemoteBetService.instance)
     private var items: [Bet] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Odds"
-        
+
         let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
         let layout = UICollectionViewCompositionalLayout.list(using: configuration)
         list = UICollectionView(frame: view.frame, collectionViewLayout: layout)
-        
+
         list.register(UICollectionViewListCell.self, forCellWithReuseIdentifier: "cell_id")
-        
+
         list.dataSource = self
         list.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(list)
@@ -27,17 +27,17 @@ class ViewController: UIViewController, UICollectionViewDataSource {
             list.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             list.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             list.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            list.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            list.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
-        
+
         activity = UIActivityIndicatorView(style: .medium)
         activity.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(activity)
         NSLayoutConstraint.activate([
             activity.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            activity.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activity.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
-        
+
         UIView.animate(withDuration: 1, animations: {
             self.list.isHidden = true
             self.activity.startAnimating()
@@ -58,11 +58,11 @@ class ViewController: UIViewController, UICollectionViewDataSource {
             }
         }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return items.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let item = items[indexPath.item]
         let cell = list.dequeueReusableCell(withReuseIdentifier: "cell_id", for: indexPath) as? UICollectionViewListCell
